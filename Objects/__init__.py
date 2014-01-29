fb792b49e7f4b048fe51873642dbc662a037bf0f
Xeom <fwharf.fw@googlemail.com>
2014-01-29 11:11:34 +0000

    Added better pathin'

--- Objects/__init__.py
+++ Objects/__init__.py
@@ -9,20 +9,20 @@ def Load(file, variables):
         print("Error loading "+file+": "+str(E))
 
 def LoadScripts(path, variables):
-    cwd     = os.getcwd()+path
 
-    fileList = [cwd+p for p in os.listdir(cwd) if p.endswith(".py")]
+    fileList = [path+p for p in os.listdir(path) if p.endswith(".py")]
 
     try:
-        dependencies = open(cwd+"Dependencies.txt").readlines()
+        dependencies = open(path+"Dependencies.txt").readlines()
 
     except Exception as e:
-        print("Objects/Dependencies.txt not found."+str(e))
+        print(path+"/Dependencies.txt not found."+str(e))
         dependencies = ''
 
-    for file in (cwd+p.strip() for p in dependencies):
+    for file in (path+p.strip() for p in dependencies):
         if file in fileList:
             fileList.remove(file)
+        
         if file[0] != '!':
             Load(file, variables)
 
