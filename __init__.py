44d61b30a5153f6b8641e0f88e2ba87e33e9e3e4
Xeom <fwharf.fw@googlemail.com>
2014-01-30 14:24:56 +0000

    kmujnyhbtgvrfcxedswz

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@ import Manager
 import traceback
 
 LIBS = "/Objects/"
+TPS  = 60
 
 GAME = input("Game directory:")
 
 try:
-    Manager.Manager(LIBS, GAME)
+    Manager.Manager(LIBS, GAME, TPS)
 
 except Exception as E:
     print("Error encounted!")
