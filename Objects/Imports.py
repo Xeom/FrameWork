146c99acde5c9d9829bc3bbc62c8bbca9fac48ec
Xeom <fwharf.fw@googlemail.com>
2014-02-05 11:16:36 +0000

    Documentation.

--- Objects/Imports.py
+++ Objects/Imports.py
@@ -2,6 +2,10 @@ import math as Math
 from random import randint as Rand
 
 def GetColour(colour):
+    """Convert a colour from either the 0xRRGGBB, "RRGGBB" or "RRGGBBAA" to a tuple.
+
+    colour is the object to convert"""
+    
     if isinstance(colour, str):
         return list((int(colour[x:x+2], 16)\
                      for x in range(0, len(colour), 2)))
@@ -19,6 +23,11 @@ def GetColour(colour):
         return colour
 
 def IsIn(pos, rect):
+    """Reterns if a position is in a rect.
+    
+    pos is the position to check.
+    rect is the rect to check if the pos is in."""
+    
     return pos[0] >= rect[0] and\
            pos[1] >= rect[1] and\
            pos[0] <= rect[0]+rect[2] and\
